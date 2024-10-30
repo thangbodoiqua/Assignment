ec22890f7ec2eb31ff70d22fcb17846798cadbca
Thangbodoiqua <dluy2406@gmail.com>
2024-10-30 09:39:57 +0700

    add and change something but still not work

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
-const mysql = require('mysql2');
+const { engine } = require('express-handlebars');  // Sử dụng cú pháp destructuring để import engine
+// const mysql = require('mysql2');
 const cookieParser = require('cookie-parser');
 const path = require('path');
 
@@ -8,54 +8,61 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/resources', express.static(path.join(__dirname, 'resources')));
 // Cấu hình Handlebars
-app.engine('handlebars', exphbs());
+app.engine('handlebars', engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
 
-// Kết nối cơ sở dữ liệu
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'wpr',
-  password: 'fit2024',
-  database: 'wpr2201040168', // Thay <sid> bằng mã sinh viên của bạn
-});
+// // Kết nối cơ sở dữ liệu
+// const db = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'wpr',
+//   password: 'fit2024',
+//   database: 'wpr<sid>', // Thay <sid> bằng mã sinh viên của bạn
+// });
 
-db.connect(err => {
-  if (err) throw err;
-  console.log('Connected to MySQL');
-});
+// db.connect(err => {
+//   if (err) throw err;
+//   console.log('Connected to MySQL');
+// });
 
 // Route trang Sign-in
 app.get('/', (req, res) => {
-  res.render('signin', { error: null });
+  res.render('signin', { title: 'Sign In', error: null });
 });
 
-// Route xử lý đăng nhập
-app.post('/signin', (req, res) => {
-  const { email, password } = req.body;
-
-  db.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], (err, results) => {
-    if (err) throw err;
-
-    if (results.length > 0) {
-      res.cookie('session', 'userLoggedIn', { httpOnly: true });
-      res.redirect('/inbox');
-    } else {
-      res.render('signin', { error: 'Invalid email or password' });
-    }
-  });
+// Route trang Sign-up
+app.get('/signup', (req, res) => {
+  res.render('signup', { title: 'Sign Up', error: null });
 });
 
-app.get('/inbox', (req, res) => {
-  if (!req.cookies.session) {
-    return res.status(403).send('Access Denied');
-  }
-  res.send('This is the inbox page');
-});
+// // Route xử lý form đăng ký (Sign-up)
+// app.post('/signup', (req, res) => {
+//   const { fullName, email, password, repassword } = req.body;
+
+//   if (password !== repassword) {
+//     return res.render('signup', { title: 'Sign Up', error: 'Passwords do not match' });
+//   }
+
+//   const query = `INSERT INTO users (full_name, email, password) VALUES (?, ?, ?)`;
+//   db.query(query, [fullName, email, password], (err, result) => {
+//     if (err) {
+//       if (err.code === 'ER_DUP_ENTRY') {
+//         res.render('signup', { title: 'Sign Up', error: 'Email is already in use' });
+//       } else {
+//         throw err;
+//       }
+//     } else {
+//       res.redirect('/');
+//     }
+//   });
+// });
 
 const PORT = 8000;
 app.listen(PORT, () => {
+  var a = 1;
+  var b = 2;
+  var c = a + b;
   console.log(`Server running at http://localhost:${PORT}/`);
 });
